eed8018757c27b353af6e08bdf15f8118424e8ac
Artem Potehin <potiekhin-01@mail.ru>
2024-07-09 22:06:30 +0300

    Home Work 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import kotlinx.coroutines.coroutineScope
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -46,6 +48,11 @@ dependencies {
 
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("androidx.cardview:cardview:1.0.0")
+
+    //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    //implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+   // implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    //implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
--- app/src/main/java/com/example/premierleaguefixture/MainActivity.kt
+++ app/src/main/java/com/example/premierleaguefixture/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.premierleaguefixture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.premierleaguefixture.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var navController: NavController
+
+    val MatchesAPI = MatchesAPI()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,6 +27,12 @@ class MainActivity : AppCompatActivity() {
 
         navController= Navigation.findNavController(this, R.id.fragment)
         MAIN = this
+
+        lifecycleScope.launch {
+            MatchesAPI.getData().collect{ data ->
+                Log.d("MyLog1", "Данные: $data")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
